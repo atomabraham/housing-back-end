daca8cbb413e0422bc99d88796ae9faa31d18ba0
atomabraham <tadzongmbipeabraham2004@fmail.com>
2023-08-03 08:44:18 +0100

    commmit with AUthenticate system

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('userName')->nullable();
+            $table->string('name')->nullable();
+            $table->string('secondname')->nullable();
+            $table->string('address')->nullable();
+            $table->integer('phone')->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->string('postalCode')->nullable();
+            $table->string('role')->default('User');
+            $table->string('status')->default('enable');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
